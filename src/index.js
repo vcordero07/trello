12f9c883a0e235825806f71f748e626663775911
vcordero07 <vcordero07@gmail.com>
2017-11-28 23:21:46 -0500

    wip

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ import ReactDOM from 'react-dom';
+ import Board from './components/board'; // Imported the Board component
+ import './index.css';
+
+ ReactDOM.render(
+   <Board />, // Rendered the Board component
+   document.getElementById('root')
+ );
